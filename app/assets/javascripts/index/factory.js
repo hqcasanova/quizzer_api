b33bae08b3bb488cb36f5921d138b052fb81bebb
hqcasanova <hqcasanova@users.noreply.github.com>
2014-10-05 00:37:39 -0400

    Debugged IndexFactory's network related variables and functions. Added br's to show view.

--- app/assets/javascripts/index/factory.js
+++ app/assets/javascripts/index/factory.js
@@ -13,27 +13,27 @@
 
             var _this = this;
 
-            this.url = indexUrl;
-            this.params = angular.extend (_this.params, {page: _this.pageNum});
             this.items = [ ];
             this.pageNum = 1;
             this.isLastPage = false;
             this.getPage = getPage;
+            this.url = indexUrl;
+            this.params = indexParams;
 
             //Updates page counter and flags when the last page has been retrieved.
             function addPage(list, page) {
                 _this.isLastPage = page.pop();    //last item = comparison on the server between page number and number of pages
-                list.items = list.items.concat(page)
+                list.items = list.items.concat(page);
                 _this.pageNum += 1;
             };
 
             //Grabs the paginated, JSON-formatted page of quizzes from the server and adds it to the list.
             function getPage(list) {
-                $http.get(
-                    url: _this.indexUrl, 
+                $http({
+                    url: _this.url, 
                     method: "GET",
-                    params: _this.params
-                ).success(function(data) {
+                    params: angular.extend (_this.params, {page: this.pageNum})
+                }).success(function(data) {
                     addPage(list, data);
                 }).error(function() {
                     alert('Problem encountered while fetching the JSON list of quizzes');
